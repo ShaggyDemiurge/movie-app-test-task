bed73462a2e6ed9d85ea3460d67057a1e5631dd9
Ilya Khegay <i.v.khegay@gmail.com>
2022-11-09 15:41:36 +0700

    MovieCard stubs

--- core/src/main/kotlin/com/github/shaggydemiurge/movieapp/core/entities/MovieSummary.kt
+++ core/src/main/kotlin/com/github/shaggydemiurge/movieapp/core/entities/MovieSummary.kt
@@ -3,6 +3,7 @@ package com.github.shaggydemiurge.movieapp.core.entities
 import java.time.LocalDate
 
 data class MovieSummary(
+    val id: String,
     val title: String,
     val posterUri: String,
     val avgScore: Float?,
--- presentation/src/main/kotlin/com/github/shaggydemiurge/movieapp/presentation/screen/list/MovieListView.kt
+++ presentation/src/main/kotlin/com/github/shaggydemiurge/movieapp/presentation/screen/list/MovieListView.kt
@@ -1,9 +1,16 @@
 package com.github.shaggydemiurge.movieapp.presentation.screen.list
 
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.github.shaggydemiurge.movieapp.core.entities.MovieSummary
 import com.github.shaggydemiurge.movieapp.presentation.common.LoadMoreHandler
 import org.koin.androidx.compose.koinViewModel
 
@@ -15,13 +22,34 @@ fun MovieListView(
 ) {
     val lazyListState = rememberLazyListState()
 
+    val movieList by viewModel.movieList
+    val listLoading by viewModel.listLoading
+
     LoadMoreHandler(lazyListState, buffer = 4) {
         viewModel.loadMore()
     }
-
     LazyColumn(
         modifier = modifier,
         state = lazyListState
     ) {
+        val itemModifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .defaultMinSize(minHeight = 100.dp)
+        items(movieList, key = { it.id }) { item ->
+            MovieCard(movieSummary = item, modifier = itemModifier, onClick = { onMovieSelect(item.id) })
+        }
+        if (listLoading) {
+            Loader(modifier = itemModifier)
+        }
     }
+}
+
+@Composable
+fun MovieCard(movieSummary: MovieSummary, modifier: Modifier = Modifier, onClick: (MovieSummary) -> Unit) {
+    // TODO
+}
+
+fun Loader(modifier: Modifier = Modifier) {
+    // TODO
 }
