ad015362dbd8016ddd2fcf4e180e705beb5a1717
Ilya Khegay <i.v.khegay@gmail.com>
2022-11-10 11:21:26 +0700

    Decouple paginator from delegate

--- presentation/src/main/kotlin/com/github/shaggydemiurge/movieapp/presentation/common/paginator/ImmutablePaginator.kt
+++ presentation/src/main/kotlin/com/github/shaggydemiurge/movieapp/presentation/common/paginator/ImmutablePaginator.kt
@@ -3,19 +3,19 @@ package com.github.shaggydemiurge.movieapp.presentation.common.paginator
 @Suppress("MemberVisibilityCanBePrivate")
 data class ImmutablePaginator<Model, State : ImmutablePaginator.PaginationState<Model>>(
     val state: State,
-    private val onLoadMore: suspend (State) -> State,
-    private val onReplaceData: (State, newData: List<Model>) -> State,
-    private val isRequestRequired: (State) -> Boolean = { true },
+    private val delegate: Delegate<Model, State>,
 ) {
 
     val data get() = state.data
 
     suspend fun requestMore(): ImmutablePaginator<Model, State> {
-        if (!isRequestRequired(state)) return this
-        return copy(state = onLoadMore(state))
+        with(delegate) {
+            if (!state.isRequestRequired()) return this@ImmutablePaginator
+            return copy(state = state.loadMore())
+        }
     }
 
-    fun replaceDataset(data: List<Model>) = copy(state = onReplaceData(state, data))
+    fun replaceDataset(data: List<Model>) = with(delegate) { copy(state = state.replaceData(data)) }
 
     fun updateItem(newItem: Model, oldItemSelector: (Model) -> Boolean) = replaceDataset(
         data.map { if (oldItemSelector(it)) newItem else it }
@@ -23,5 +23,12 @@ data class ImmutablePaginator<Model, State : ImmutablePaginator.PaginationState<
 
     interface PaginationState<Model> {
         val data: List<Model>
+    }
+
+    interface Delegate<Model, State> {
+
+        suspend fun State.loadMore(): State
+        fun State.replaceData(newData: List<Model>): State
+        fun State.isRequestRequired(): Boolean = true
     }
 }
--- presentation/src/main/kotlin/com/github/shaggydemiurge/movieapp/presentation/common/paginator/PagePaginator.kt
+++ presentation/src/main/kotlin/com/github/shaggydemiurge/movieapp/presentation/common/paginator/PagePaginator.kt
@@ -5,25 +5,33 @@ import com.github.shaggydemiurge.movieapp.core.entities.PagedResult
 object PagePaginator {
 
     operator fun <Model> invoke(startPage: Int = 0, onLoadMore: suspend (page: Int) -> PagedResult<Model>) =
-        ImmutablePaginator<Model, State<Model>>(
+        ImmutablePaginator(
             state = State(emptyList(), startPage - 1, null),
-            onLoadMore = { oldState ->
-                val result = onLoadMore(oldState.currentLoadedPage + 1)
-                if (result.data.isNotEmpty()) {
-                    State(
-                        oldState.data + result.data,
-                        result.page,
-                        result.maxPages
-                    )
-                } else {
-                    oldState
-                }
-            },
-            onReplaceData = { state, newData -> state.copy(data = newData) },
-            isRequestRequired = { state ->
-                state.maxPages == null || state.currentLoadedPage < state.maxPages
-            }
+            delegate = Delegate(onLoadMore)
         )
+
+    class Delegate<Model>(private val onLoadMore: suspend (page: Int) -> PagedResult<Model>) :
+        ImmutablePaginator.Delegate<Model, State<Model>> {
+        override suspend fun State<Model>.loadMore(): State<Model> {
+            val result = onLoadMore(currentLoadedPage + 1)
+            return if (result.data.isNotEmpty()) {
+                State(
+                    data + result.data,
+                    result.page,
+                    result.maxPages
+                )
+            } else {
+                this
+            }
+        }
+
+        override fun State<Model>.replaceData(newData: List<Model>): State<Model> = copy(
+            data = newData
+        )
+
+        override fun State<Model>.isRequestRequired(): Boolean =
+            maxPages == null || currentLoadedPage < maxPages
+    }
 
     data class State<Model>(override val data: List<Model>, val currentLoadedPage: Int, val maxPages: Int?) :
         ImmutablePaginator.PaginationState<Model>
